3561b4801b8dec4053486cbd4605da643ef182c3
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-03-16 18:29:01 +0400

    get store data, show hide logged button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {Observable} from "rxjs";
-import {map} from 'rxjs/operators';
-import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { AuthState } from './auth/reducers';
 
 @Component({
   selector: 'app-root',
@@ -11,37 +12,51 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 })
 export class AppComponent implements OnInit {
 
-    loading = true;
+  loading = true;
 
-    constructor(private router: Router) {
+  isLoggedIn$: Observable<boolean>;
 
-    }
+  isLoggedOut$: Observable<boolean>;
 
-    ngOnInit() {
+  constructor(private router: Router, private store: Store<AuthState>) {
 
-      this.router.events.subscribe(event  => {
-        switch (true) {
-          case event instanceof NavigationStart: {
-            this.loading = true;
-            break;
-          }
+  }
 
-          case event instanceof NavigationEnd:
-          case event instanceof NavigationCancel:
-          case event instanceof NavigationError: {
-            this.loading = false;
-            break;
-          }
-          default: {
-            break;
-          }
+  ngOnInit() {
+
+    this.router.events.subscribe(event => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
         }
-      });
 
-    }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
 
-    logout() {
+    this.isLoggedIn$ = this.store
+      .pipe(
+        map(state => !!state["auth"].user)
+      );
 
-    }
+    this.isLoggedOut$ = this.store
+      .pipe(
+        map(state => !state["auth"].user)
+      );
+
+  }
+
+  logout() {
+
+  }
 
 }
