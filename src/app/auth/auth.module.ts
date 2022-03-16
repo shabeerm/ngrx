f16e39ac163d6d18ac925426a3fef0551a640019
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-03-16 18:06:35 +0400

    save dispatched user data to store state

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,6 +10,7 @@ import { StoreModule } from '@ngrx/store';
 import {AuthService} from "./auth.service";
 import { EffectsModule } from '@ngrx/effects';
 import * as fromAuth from './reducers';
+import { authReducer } from './reducers';
 
 @NgModule({
     imports: [
@@ -19,7 +20,7 @@ import * as fromAuth from './reducers';
         MatInputModule,
         MatButtonModule,
         RouterModule.forChild([{path: '', component: LoginComponent}]),
-        StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducers),
+        StoreModule.forFeature(fromAuth.authFeatureKey, authReducer),
 
     ],
     declarations: [LoginComponent],
--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -2,16 +2,31 @@ import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  on
 } from '@ngrx/store';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
 
 export const authFeatureKey = 'auth';
 
 export interface AuthState {
+  user: User;
 
 }
 
-export const reducers: ActionReducerMap<AuthState> = {
+export const initialAuthState: AuthState = {
+  user: undefined
+}
 
-};
+export const authReducer = createReducer(
+  initialAuthState,
+
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  })
+);
