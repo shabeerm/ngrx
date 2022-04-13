742413bec0e245819c51976379221089bfbee3f0
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-04-13 15:40:31 +0400

    added courses effect, fetching from backened

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -26,6 +26,8 @@ import {compareCourses, Course} from './model/course';
 
 import {compareLessons, Lesson} from './model/lesson';
 import { CoursesResolver } from './courses.resolver';
+import { CoursesEffects } from './courses.effects';
+import { EffectsModule } from '@ngrx/effects';
 
 
 export const coursesRoutes: Routes = [
@@ -62,7 +64,8 @@ export const coursesRoutes: Routes = [
     MatDatepickerModule,
     MatMomentDateModule,
     ReactiveFormsModule,
-    RouterModule.forChild(coursesRoutes)
+    RouterModule.forChild(coursesRoutes),
+    EffectsModule.forFeature([CoursesEffects])
   ],
   declarations: [
     HomeComponent,
