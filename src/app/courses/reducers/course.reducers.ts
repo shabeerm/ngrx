57283493e6f05f2ce806aea8cbfe5661e2f754a2
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-04-15 15:31:36 +0400

    How to Load Data Only If Needed

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,22 +1,31 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
+import { allCoursesLoaded } from "../course.actions";
 import { compareCourses, Course } from "../model/course";
 
 export interface CoursesState extends EntityState<Course> {
-
+  allCoursesLoaded: boolean
 }
 
 export const adapter = createEntityAdapter<Course>({
   sortComparer: compareCourses
 });
 
-export const initialCoursesState = adapter.getInitialState();
+export const initialCoursesState = adapter.getInitialState({
+  allCoursesLoaded: false
+});
 
 export const coursesReducer = createReducer(
   initialCoursesState,
   on(CourseActions.allCoursesLoaded,
-    (state, action) => adapter.addAll(action.courses, state)
+    (state, action) => adapter.addAll(
+      action.courses,
+      {
+        ...state,
+        allCoursesLoaded: true
+      }
+    )
   )
 )
 
