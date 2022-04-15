479ddfb6ec3603b3b69bde2a5191db560e782a7f
Shabeer Moothedath <shabeer.moothedath@maqta.ae>
2022-04-15 14:50:11 +0400

    Refactoring Home Component with courses selector

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -6,6 +6,9 @@ import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialo
 import { MatDialog } from '@angular/material/dialog';
 import {map, shareReplay} from 'rxjs/operators';
 import {CoursesHttpService} from '../services/courses-http.service';
+import { AppState } from '../../reducers';
+import { Store } from '@ngrx/store';
+import { selectAdvancedCourses, selectBeginnerCourses, selectPromoTotal } from '../courses.selectors';
 
 
 
@@ -27,7 +30,7 @@ export class HomeComponent implements OnInit {
 
     constructor(
       private dialog: MatDialog,
-      private coursesHttpService: CoursesHttpService) {
+      private store: Store<AppState>) {
 
     }
 
@@ -37,29 +40,9 @@ export class HomeComponent implements OnInit {
 
   reload() {
 
-    const courses$ = this.coursesHttpService.findAllCourses()
-      .pipe(
-        map(courses => courses.sort(compareCourses)),
-        shareReplay()
-      );
-
-    this.loading$ = courses$.pipe(map(courses => !!courses));
-
-    this.beginnerCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'BEGINNER'))
-      );
-
-
-    this.advancedCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'ADVANCED'))
-      );
-
-    this.promoTotal$ = courses$
-        .pipe(
-            map(courses => courses.filter(course => course.promo).length)
-        );
+    this.beginnerCourses$ = this.store.select(selectBeginnerCourses);
+    this.advancedCourses$ = this.store.select(selectAdvancedCourses);
+    this.promoTotal$ = this.store.select(selectPromoTotal);
 
   }
 
